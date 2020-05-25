55d62c2d8b6ce8ea7e62bd5d8f7c30036984223e
tarunmenon <tarunmenon15@gmail.com>
2020-05-25 17:26:12 +0530

    Add files via upload

---
+++ Getting_Elevation.R
@@ -0,0 +1,79 @@
+setwd("D:/Post_NCBS/NCF/migration")
+
+library(raster)
+
+## use getData to download DEM for India and save working directory (download = F after first time)
+## download rasters for countries of interest
+
+x<-getData('alt', country = "IND", download = F, mask = F)
+z<-getData('alt', country = "NPL", download = F, mask = F)
+bb<-raster::merge(x,z)
+
+readcleanrawdata = function(rawpath)
+{
+  require(lubridate)
+  require(tidyverse)
+  require(cowplot)
+  
+  #library(auk)
+  
+  #allin = system.file("extdata/ebd_IN_relMay-2018.txt", package = "auk")
+  
+  #allout = tempfile()
+  #auk_clean(allin,allout, sep = "\t", remove_text = FALSE)
+  
+  #all = allout %>%
+  #  read_ebd()
+  
+  preimp = c("COMMON.NAME","OBSERVATION.COUNT",
+             "LOCALITY.ID","LOCALITY.TYPE",
+             "LATITUDE","LONGITUDE","OBSERVATION.DATE","TIME.OBSERVATIONS.STARTED","OBSERVER.ID",
+             "PROTOCOL.TYPE","DURATION.MINUTES","EFFORT.DISTANCE.KM",
+             "NUMBER.OBSERVERS","ALL.SPECIES.REPORTED","GROUP.IDENTIFIER","SAMPLING.EVENT.IDENTIFIER","APPROVED","CATEGORY")
+  
+  nms = read.delim(rawpath, nrows = 1, sep = "\t", header = T, quote = "", stringsAsFactors = F, na.strings = c(""," ",NA))
+  nms = names(nms)
+  nms[!(nms %in% preimp)] = "NULL"
+  nms[nms %in% preimp] = NA
+  
+  data = read.delim(rawpath, colClasses = nms, sep = "\t", header = T, quote = "", stringsAsFactors = F, na.strings = c(""," ",NA))
+  
+  ## choosing important variables
+  
+  imp = c("COMMON.NAME","OBSERVATION.COUNT",
+          "LOCALITY.ID","LOCALITY.TYPE",
+          "LATITUDE","LONGITUDE","OBSERVATION.DATE","TIME.OBSERVATIONS.STARTED","OBSERVER.ID",
+          "PROTOCOL.TYPE","DURATION.MINUTES","EFFORT.DISTANCE.KM",
+          "NUMBER.OBSERVERS","ALL.SPECIES.REPORTED","group.id")
+  
+  days = c(31,28,31,30,31,30,31,31,30,31,30,31)
+  cdays = c(0,31,59,90,120,151,181,212,243,273,304,334)
+  
+  ## setup eBird data ##
+  
+  ## filter approved observations, species, slice by single group ID, remove repetitions
+  ## remove repeats
+  ## set date, add month, year and day columns using package LUBRIDATE
+  ## add number of species column (no.sp)
+  
+  data = data %>%
+    filter(APPROVED == 1) %>%
+    mutate(group.id = ifelse(is.na(GROUP.IDENTIFIER), SAMPLING.EVENT.IDENTIFIER, GROUP.IDENTIFIER)) %>%
+    group_by(group.id) %>% slice(1) %>% ungroup %>%
+    dplyr::select(imp) %>%
+    mutate(OBSERVATION.DATE = as.Date(OBSERVATION.DATE), 
+           month = month(OBSERVATION.DATE), year = year(OBSERVATION.DATE),
+           day = day(OBSERVATION.DATE) + cdays[month], week = week(OBSERVATION.DATE),
+           fort = ceiling(day/14)) %>%
+    ungroup
+  
+  dat<-subset(data,select = c("LATITUDE","LONGITUDE")) %>% distinct(.keep_all = T)
+  dat<-dat[,c(2,1)]
+  dat<-cbind(dat, alt = raster::extract(bb, dat))
+  
+  data_with_alt<-merge(data,dat, by = c("LATITUDE","LONGITUDE"))
+  #data_with_alt$alt[is.na(data_with_alt$alt)] <- 0
+  
+  
+  return(data_with_alt)
+  
